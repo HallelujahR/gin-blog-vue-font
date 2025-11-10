53d9fc4fff8b36a506148d968bfc3f48e4e2ae46
wangruiwen <13001130055@163.com>
2025-11-10 18:46:20 +0800

    测试

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { adminAuth } from '../utils/auth.js';
 
 const http = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: '/api',
   timeout: 7000,
 });
 
